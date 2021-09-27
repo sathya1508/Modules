7a3e6928a33272bce1c49f33d64d8e7d743ff412
sathya1508 <sathya1508@gmail.com>
2021-09-27 12:03:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8zjvof
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8zjvof)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+}
+
+// Structure of module
+// different architectural structures
+// 1. Use modules create functions 
+// 2. 
