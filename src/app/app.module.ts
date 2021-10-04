a56f34a3605b6f0be6bd74cd95dbe65654648585
sathya1508 <sathya1508@gmail.com>
2021-10-04 20:08:17 +0530

    queryParams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+const routes: Routes = [
+  { path: 'home', component: HelloComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+
+  constructor(private activatedRoutes: ActivatedRoute) {
+    this.activatedRoutes.queryParams.subscribe((params) => {
+      console.log(params);
+    });
+  }
 }
